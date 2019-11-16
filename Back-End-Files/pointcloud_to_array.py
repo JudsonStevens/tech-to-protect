c273d5347e9a6fc2904ec2b059b29adee04f1ca9
Judson Stevens <Judson.Stevens@gmail.com>
2019-11-15 18:56:23 -0700

    Moved back end files

--- Back-End-Files/pointcloud_to_array.py
+++ Back-End-Files/pointcloud_to_array.py
@@ -4,18 +4,22 @@ import matplotlib.pyplot as plt
 import open3d as o3d
 import sys
 
-def main(filepath):
-  pcd = o3d.io.read_point_cloud(filepath[0], format='xyz')
+def main(argument):
+  pcd = o3d.io.read_point_cloud(argument[0], format='xyz')
   xyz_load = np.asarray(pcd.points)
   x_lidar = xyz_load[:, 0]
   y_lidar = xyz_load[:, 1]
   z_lidar = xyz_load[:, 2]
   min_value = min(z_lidar)
   max_value = max(z_lidar)
+  original_size = z_lidar.size
+  if argument[1]:
+    step = float(argument[1])
+  else:
+    step = 0.5
   while min_value < max_value:
-    max_value = min_value + 1
-    birds_eye_point_cloud(x_lidar, y_lidar, z_lidar, min_value, max_value)
-    min_value += 1
+    birds_eye_point_cloud(x_lidar, y_lidar, z_lidar, min_value, max_value, step, original_size)
+    min_value = min_value + step
 
 # ==============================================================================
 #                                                                   SCALE_TO_255
@@ -34,36 +38,42 @@ def birds_eye_point_cloud(
                           z_lidar,
                           min_value,
                           max_value,
+                          step,
+                          original_size,
                           side_range=(-10, 10),
                           fwd_range=(-10,10),
                           res=0.001,
-                          min_height = -0.5,
-                          max_height = 0.5,
-                          saveto='/home/judson/Documents/example_img#'):
+                          saveto='/home/judson/Documents/Sliced-Pictures-Comparison/example_img-min_size-'):
   """ Creates an 2D birds eye view representation of the point cloud data.
       You can optionally save the image to specified filename.
+
+      The side and fwd range values can be modified if needed in order to increase or decrease the size of the image.
   """
 
-  # r_lidar = points[:, 3]  # Reflectance
-  # min_value = min(z_lidar)
-  # max_value = max(z_lidar)
   print(f'Minimum value = {min_value}, maximum value = {max_value}')
-  first_set = np.where(z_lidar > max_value)
+
+  # Gather all indices where the z values are outside the range we are specifying by the step.
+  first_set = np.where(z_lidar > min_value + step)
   second_set = np.where(z_lidar < min_value)
   indices_to_delete = np.setxor1d(first_set, second_set)
-  # indices_to_delete = np.where(np.logical_and((z_lidar > (max_value - 3.5)), (z_lidar < min_value + 3)))
+
+  # Check to make sure the delete process worked.
   if len(indices_to_delete) == 0:
     raise Exception("No indices were found to delete.")
-  print(indices_to_delete)
+
+  # Print out the length of the original z list, and the size of the array of indices to delete.
   print(len(z_lidar))
   print(len(indices_to_delete))
   x_lidar = np.delete(x_lidar, indices_to_delete)
   y_lidar = np.delete(y_lidar, indices_to_delete)
   z_lidar = np.delete(z_lidar, indices_to_delete)
 
+  # Print out the final length of the z point array.
   print(len(z_lidar))
+
   # INDICES FILTER - of values within the desired rectangle
   # Note left side is positive y axis in LIDAR coordinates
+  # This should be modified depending on the image.
   ff = np.logical_and((x_lidar > fwd_range[0]), (x_lidar < fwd_range[1]))
   ss = np.logical_and((y_lidar > -side_range[1]), (y_lidar < -side_range[0]))
   indices = np.argwhere(np.logical_and(ff,ss)).flatten()
@@ -80,57 +90,26 @@ def birds_eye_point_cloud(
   
   # CLIP HEIGHT VALUES - to between min and max heights
   pixel_values = np.clip(a = z_lidar[indices],
-                          a_min=min_height,
-                          a_max=max_height)
+                          a_min=min_value,
+                          a_max=(min_value + step))
+
   # RESCALE THE HEIGHT VALUES - to be between the range 0-255
-  pixel_values  = scale_to_255(pixel_values, min=min_height, max=max_height)
+  pixel_values  = scale_to_255(pixel_values, min=min_value, max=(min_value + step))
 
   # FILL PIXEL VALUES IN IMAGE ARRAY
   x_max = int((side_range[1] - side_range[0])/res)
   y_max = int((fwd_range[1] - fwd_range[0])/res)
   im = np.zeros([y_max, x_max], dtype=np.uint8)
   im[-y_img, x_img] = pixel_values # -y because images start from top left
-  # im = np.clip()
-  # return im
+
+  print(z_lidar.size)
+  print(original_size)
+  if im.size > (original_size * .15):
   # Convert from numpy array to a PIL image
-  im = Image.fromarray(im)
-
-  # import code; code.interact(local=dict(globals(), **locals()))
-  im.save(saveto + f'{round(min_value, 2)}.jpeg')
-  # SAVE THE IMAGE
-  # if saveto is not None:
-
-  # else:
-  #   im.show()
+    im = Image.fromarray(im)
+    im.save(saveto + f'{round(min_value, 2)}.jpeg')
+  else:
+    print("Array was too small.")
 
 if __name__ == "__main__":
   main(sys.argv[1:])
-
-'''
-  Args:
-      points:     (numpy array)
-                  N rows of points data
-                  Each point should be specified by at least 3 elements x,y,z
-      side_range: (tuple of two floats)
-                  (-left, right) in metres
-                  left and right limits of rectangle to look at.
-      fwd_range:  (tuple of two floats)
-                  (-behind, front) in metres
-                  back and front limits of rectangle to look at.
-      res:        (float) desired resolution in metres to use
-                  Each output pixel will represent an square region res x res
-                  in size.
-      min_height:  (float)(default=-2.73)
-                  Used to truncate height values to this minumum height
-                  relative to the sensor (in metres).
-                  The default is set to -2.73, which is 1 metre below a flat
-                  road surface given the configuration in the kitti dataset.
-      max_height: (float)(default=1.27)
-                  Used to truncate height values to this maximum height
-                  relative to the sensor (in metres).
-                  The default is set to 1.27, which is 3m above a flat road
-                  surface given the configuration in the kitti dataset.
-      saveto:     (str or None)(default=None)
-                  Filename to save the image as.
-                  If None, then it just displays the image.
-'''
